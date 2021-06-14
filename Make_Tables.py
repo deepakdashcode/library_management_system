3714d1635754bf587bce5d53ba157b30b8ddc0a3
Deepak Kumar Dash <dipudash.2003@gmail.com>
2021-06-14 22:07:21 +0530

    First Commit

--- Make_Tables.py
+++ Make_Tables.py
@@ -25,17 +25,6 @@ def create_table1():
 
 create_table1()
 
-# def create_table1():
-#     db=c.connect(host='localhost', user='root', passwd='2580',database='my_library')
-#     cur=db.cursor()
-#     sql='''create table books
-#     ( ISBN int(15) NOT NULL , BOOK_NAME varchar(100) NOT Null, std varchar(10), Quantity integer(5) NOT NULL,
-#     CONSTRAINT books_pk PRIMARY KEY (ISBN)
-#     );
-#     '''
-#     cur.execute(sql)
-
-
 def create_table2():
     db=c.connect(host='localhost', user='root', passwd='2580',database='my_library')
     cur=db.cursor()
